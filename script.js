83814c4086ceac39e83d3e9733ef535e27931879
Jake Schmidt <jakeschmidt213@gmail.com>
2025-03-02 15:11:33 -0700

    Script Setup

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const passwordLength = 10;
+const includeUpperCase = true;
+const includeLowerCase = true;
+const includeNumbers = true;
+const includeSymbols = true;
 
+function generatePassword(length, includeUpperCase, includeLowerCase, includeNumbers, includeSymbols) {
+    return '';
+}
+
+let password = generatePassword(passwordLength, includeUpperCase, includeLowerCase, includeNumbers, includeSymbols);
+
+
+console.log(`Generated Password: ${password}`);
 
 
