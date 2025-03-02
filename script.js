3cd77f57087b2fd42cc4ab8874e6e6adbf252800
Jake Schmidt <jakeschmidt213@gmail.com>
2025-03-02 15:19:59 -0700

    Generates Password\Console

--- script.js
+++ script.js
@@ -5,7 +5,33 @@ const includeNumbers = true;
 const includeSymbols = true;
 
 function generatePassword(length, includeUpperCase, includeLowerCase, includeNumbers, includeSymbols) {
-    return '';
+
+    const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    const numberChars = '0123456789';
+    const symbolChars = '!@#$%^&*()_+-=[]{}|;:,.<>/?';
+
+    let allowedChars = '';
+    let password = '';
+
+    allowedChars += includeUpperCase ? uppercaseChars : '';
+    allowedChars += includeLowerCase ? lowercaseChars : '';
+    allowedChars += includeNumbers ? numberChars : '';
+    allowedChars += includeSymbols ? symbolChars : '';
+
+    if (length <= 0) {  
+        return 'Length must be greater than 0';
+    }
+    if (allowedChars.length === 0) {
+        return 'Please select at least one character type';
+    }
+
+    for(let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * allowedChars.length);
+        password += allowedChars[randomIndex];
+    }
+    console.log(allowedChars);
+    return password;
 }
 
 let password = generatePassword(passwordLength, includeUpperCase, includeLowerCase, includeNumbers, includeSymbols);
